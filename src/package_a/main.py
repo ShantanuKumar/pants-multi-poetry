85afaf0f5b5aa18f47a22a5965c8236cbd14664c
ShantanuKumar <Shantanu.Kumar@emobilitydv.com>
2022-08-09 18:57:44 +0200

    multi stage pants docker build

--- src/package_a/main.py
+++ src/package_a/main.py
@@ -1,15 +1,29 @@
+from typing import Union
+
 import pandas as pd
+import uvicorn
+from fastapi import FastAPI
 
-import package_b as pb
 import package_a as pa
+import package_b as pb
+
+app = FastAPI()
 
 
-def main():
-    print("inside main")
-    print(f"package_a version: {pa.__version__}")
-    print(f"package_b version: {pb.__version__}")
-    print(f"pandas version: {pd.__version__}")
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {
+        "item_id": item_id,
+        "pa_version": pa.__version__,
+        "pb_version": pb.__version__,
+        "pd_versiob": pd.__version__,
+    }
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="0.0.0.0", port=9000)  # nosec B104
