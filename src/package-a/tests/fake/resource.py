e9f0400156b29e1396195245d1687bae1f9d6079
ShantanuKumar <Shantanu.Kumar@emobilitydv.com>
2022-09-29 18:58:44 +0200

    add fake resource

---
+++ src/package-a/tests/fake/resource.py
@@ -0,0 +1,1 @@
+PANTS_VERSION = "2.14.0rc1"
--- src/package-a/tests/test_package_a.py
+++ src/package-a/tests/test_package_a.py
@@ -1,5 +1,6 @@
 from package_a import __version__
-
+from tests.fake.resource import PANTS_VERSION
 
 def test_version():
     assert __version__ == "0.1.0"
+    assert PANTS_VERSION == "2.14.0rc1"
