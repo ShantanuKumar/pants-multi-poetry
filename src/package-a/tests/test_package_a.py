9ec6ce27f4665d892932aeb95285d029415d8074
ShantanuKumar <Shantanu.Kumar@emobilitydv.com>
2022-09-29 18:43:12 +0200

    restructure to repro

--- src/package-a/tests/test_package_a.py
+++ src/package-a/tests/test_package_a.py
@@ -1,7 +1,5 @@
-import package_b
 from package_a import __version__
 
 
 def test_version():
     assert __version__ == "0.1.0"
-    assert package_b.__version__ == "0.1.0"
--- src/tests/test_package_b.py
+++
@@ -1,9 +0,0 @@
-# from package_b import __version__
-import pandas as pd
-
-import package_b
-
-
-def test_version():
-    assert package_b.__version__ == "0.1.0"
-    assert pd.__version__ is not None
