0789377b1a946ce04293555c36872479c3fe10d1
ShantanuKumar <Shantanu.Kumar@emobilitydv.com>
2023-01-21 13:26:31 +0100

    test coverage report

--- src/package-a/tests/test_package_a.py
+++ src/package-a/tests/test_package_a.py
@@ -6,6 +6,3 @@ import importlib
 def test_version():
     assert __version__ == "0.1.0"
     assert PANTS_VERSION == "2.14.0rc1"
-
-    gdcm_loaded = importlib.import_module("gdcm")
-    assert "ASN1_ParseDump" in dir(gdcm_loaded)
